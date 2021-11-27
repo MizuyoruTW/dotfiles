f31157a008399b94ddd9c89973a7d77790c1876f
MizuyoruTW <mizuyoru.tw@gmail.com>
2021-11-27 22:58:56 +0800

    change random logic to shuffling to avoid two duplicate wallpaper

--- Project/bash/random_wallpaper.sh
+++ Project/bash/random_wallpaper.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+readarray file_list < <(find ~/Pictures/wallpaper -type f)
+file_list=( $(shuf -e "${file_list[@]}") )
 while true
 do
-    feh --bg-fill $(find ~/Pictures/wallpaper -type f | shuf -n 1)
-    sleep 1800
+    for file in "${file_list[@]}"
+    do
+        feh --bg-fill "$file"
+        sleep 1800
+    done
 done
